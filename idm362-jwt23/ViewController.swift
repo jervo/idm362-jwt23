cc0bbe14b206ff5e9c8913c8fddb8c7a375c83e4
Jervis Thompson <jervo@mac.com>
2021-03-02 10:12:12 -0500

    Week 08 Animation

--- idm362-jwt23/ViewController.swift
+++ idm362-jwt23/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var polarBearAnim: UIImageView!
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do after loading.
+    polarBearAnim.animationImages = loadImages(for: "PolarBearWalking")
+    polarBearAnim.animationDuration = 2
+    polarBearAnim.animationRepeatCount = 0
+    polarBearAnim.image = polarBearAnim.animationImages?.first
+    polarBearAnim.startAnimating()
+  }
+  
+  func loadImages(for name: String) -> [UIImage] {
+    print("Animated Images Loader called")
+    var images = [UIImage]()
+    var lp = 1
+    print("image array before \(images)")
+    while let image = UIImage(named: "\(name)/\(lp)") {
+      images.append(image)
+      lp += 1
     }
-
+    print("image array after \(images)")
+    return images
+  }
 
 }
 
